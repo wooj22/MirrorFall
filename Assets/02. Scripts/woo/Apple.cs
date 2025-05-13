8fa4582eb98b65d77fbcbcfdfd6c3e8d5430800d
woojeong <77192050+wooj22@users.noreply.github.com>
2025-05-14 02:51:19 +0900

    Feat | Apple isGround flag

--- Assets/02. Scripts/woo/Apple.cs
+++ Assets/02. Scripts/woo/Apple.cs
@@ -4,9 +4,26 @@ using UnityEngine;
 
 public class Apple : MonoBehaviour
 {
+    public bool isGround = false;
+    private Vector3 previousPosition;
+    private float stopThreshold = 0.001f;
+
     private void Start()
     {
+        previousPosition = transform.position;
         Destroy(this.gameObject, 12f);  // 자가소멸
+    }
+
+    private void Update()
+    {
+        // 더이상 안움직일떄 isGournd true
+        if (!isGround)
+        {
+            float distanceMoved = (transform.position - previousPosition).sqrMagnitude;
+            if (distanceMoved < stopThreshold * stopThreshold) isGround = true;
+
+            previousPosition = transform.position;
+        }        
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
