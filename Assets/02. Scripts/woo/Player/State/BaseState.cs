29084e7989cfaf0a3e45cd4f2f9a265ba7f93c0c
Yang Ujeong <77192050+wooj22@users.noreply.github.com>
2025-05-14 19:39:55 +0900

    Feat | Player 씬 이동 (5맵)

--- Assets/02. Scripts/woo/Player/State/BaseState.cs
+++ Assets/02. Scripts/woo/Player/State/BaseState.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BaseState : MonoBehaviour
+public class BaseState
 {
     protected PlayerController player;
 
