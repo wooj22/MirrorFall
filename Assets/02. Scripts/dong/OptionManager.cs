c5339df068d6279d4e8518e794b52340104723c2
ho991127 <ho991127@naver.com>
2025-05-18 22:03:03 +0900

    Feat|스타트신 옵션 기능 추가
    옵션창 OK버튼 생성, 슬라이더 값 저장 기능 추가

--- Assets/02. Scripts/dong/OptionManager.cs
+++ Assets/02. Scripts/dong/OptionManager.cs
@@ -1,23 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OptionManager : MonoBehaviour
 {
     public GameObject OptionPanel;
+    public Slider bgmSlider;
+    public Slider vfxSlider;
+    float originalBGM;
+    float originalVFX;
 
     void Start()
     {
         OptionPanel.SetActive(false);
     }
-
-    // Update is called once per frame
     public void OnClickOptionButton()
-    {
+    {// 옵션창 켜기 전에 값 백업
+        originalBGM = PlayerPrefs.GetFloat("BGM", 0.8f);
+        originalVFX = PlayerPrefs.GetFloat("VFX", 0.8f);
+
+        bgmSlider.value = originalBGM;
+        vfxSlider.value = originalVFX;
         OptionPanel.SetActive(true);
     }
-    public void OnClickOptionCancelButton()
+    public void OnClickOKButton()
     {
+        float bgm = bgmSlider.value;
+        float vfx = vfxSlider.value;
+
+        PlayerPrefs.SetFloat("BGM", bgm);
+        PlayerPrefs.SetFloat("VFX", vfx);
+        OptionPanel.SetActive(false);
+    }
+    public void OnClickCancelButton()
+    {
+        bgmSlider.value = originalBGM;
+        vfxSlider.value = originalVFX;
+
         OptionPanel.SetActive(false);
     }
 }
