3aadea5935e6c56774d32dd50a65c88751de8aa4
ho991127 <ho991127@naver.com>
2025-05-21 16:27:11 +0900

    Feat|게임오버신 수정
    버튼 활성화, 비활성화 기능 제거, 활성화 상태 유지

--- Assets/02. Scripts/dong/ToPannelOpen.cs
+++ Assets/02. Scripts/dong/ToPannelOpen.cs
@@ -4,20 +4,10 @@ using UnityEngine;
 
 public class ToPannelOpen : MonoBehaviour
 {
-    public GameObject PannelOpenButton;
     public GameObject RetryPannel;
     void Start()
     {
-        PannelOpenButton.SetActive(false);
         RetryPannel.SetActive(false);
-        StartCoroutine(ShowPannelOpenButton());
-    }
-    IEnumerator ShowPannelOpenButton()
-    {
-        yield return new WaitForSeconds(3f);
-        PannelOpenButton.SetActive(true);
-        yield return new WaitForSeconds(8f);
-        PannelOpenButton.SetActive(false);
     }
     public void OnClickPannelOpenButton()
     {
