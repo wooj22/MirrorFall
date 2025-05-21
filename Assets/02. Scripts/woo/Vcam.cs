108d3c0c20d170ed4214aaef20e56dadc7b7ad91
Yang Ujeong <77192050+wooj22@users.noreply.github.com>
2025-05-21 20:48:26 +0900

    Fix | 보스방 재진입시 시네머신 카메라 못찾는거 예외처리

--- Assets/02. Scripts/woo/Vcam.cs
+++ Assets/02. Scripts/woo/Vcam.cs
@@ -3,9 +3,18 @@ using Cinemachine;
 
 public class Vcam : MonoBehaviour
 {
+    CinemachineVirtualCamera vcam;
+
     void Start()
     {
-        CinemachineVirtualCamera vcam = GetComponent<CinemachineVirtualCamera>();
+        vcam = GetComponent<CinemachineVirtualCamera>();
+        vcam.Follow = GameObject.FindWithTag("Player").transform;
+
+        Invoke(nameof(Test), 0.5f);
+    }
+
+    void Test()
+    {
         vcam.Follow = GameObject.FindWithTag("Player").transform;
     }
 }
