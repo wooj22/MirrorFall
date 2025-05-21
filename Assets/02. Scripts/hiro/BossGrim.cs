8df41d9501b11f365a00ff24ce8af7ada11bb280
hth-hiro <hth0114@naver.com>
2025-05-21 14:17:07 +0900

    Feat | 코드 수정
    네임스페이스 뺐어용

--- Assets/02. Scripts/hiro/BossGrim.cs
+++ Assets/02. Scripts/hiro/BossGrim.cs
@@ -1,10 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.InteropServices;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.EventSystems;
 
 public class BossGrim : MonoBehaviour
 {
