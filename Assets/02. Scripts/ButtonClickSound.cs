36011060273edbfab171a8e83dabb23b8a433b31
Yang Ujeong <77192050+wooj22@users.noreply.github.com>
2025-05-22 12:47:18 +0900

    Feat | 버튼 포인터엔터 사운드, 일시정지 패널 프리팹으로 뺌
    이제 모든 씬에 프리팹으로 다시 넣자..

--- Assets/02. Scripts/ButtonClickSound.cs
+++ Assets/02. Scripts/ButtonClickSound.cs
@@ -4,5 +4,6 @@ using UnityEngine;
 
 public class ButtonClickSound : MonoBehaviour
 {
-    public void PlayButtonSound() { SoundManager2.Instance.PlaySFX("SFX_UIButton"); }
+    public void PlayButtonSound() { SoundManager2.Instance.PlaySFX("SFX_UIButton"); }   // click
+    public void PlayButtonOnSound() { SoundManager2.Instance.PlaySFX("SFX_UIButtonSlide"); }  // cursor on
 }
